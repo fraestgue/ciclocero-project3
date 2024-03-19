751a455864497d69492dfc730b02883267e0cd9d
Francisco Estepa Guerra <estepaguerrafrancisco@gmail.com>
2024-03-19 20:10:30 +0100

    creando formruta

--- src/components/FormCrearRuta.jsx
+++ src/components/FormCrearRuta.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import service from "../service/config.service";
 
 function FormCrearRuta() {
     const navigate = useNavigate();
@@ -13,13 +14,50 @@ function FormCrearRuta() {
     const [comunidad, setComunidad] = useState();
     const [provincia, setProvincia] = useState();
     const [creador, setCreador] = useState();
+    const [image, setImage] = useState();
+    const [coordinatesStart, setCoordinatesStart] = useState();
+    const [coordinatesEnd, setCoordinatesEnd] = useState();
 
     const handleName = (event) => setName(event.target.value);
     const handleDifficulty = (event) => setDifficulty(event.target.value);
     const handleDistanciaEnKm = (event) => setDistanciaenKm(event.target.value);
     const handleDesnivelEnM = (event) => setDesnivelEnM(event.target.value);
+    const handleDuracionEnHoras = (event) =>
+        setDuracionEnHoras(event.target.value);
+    const handleModalidad = (event) => setModalidad(event.target.value);
+    const handleCircular = (event) => setCircular(event.target.value);
+    const handleComunidad = (event) => setComunidad(event.target.value);
+    const handleProvincia = (event) => setProvincia(event.target.value);
 
-    return <div>FormCrearRuta</div>;
+    const handleSubmitRuta = async (event) => {
+        event.preventDefault();
+
+        const nuevaRuta = {
+            name,
+            difficulty,
+            distanciaEnKm,
+            desnivelEnM,
+            duracionEnHoras,
+            modalidad,
+            circular,
+            comunidad,
+            provincia,
+            creador,
+            image,
+            coordinatesStart,
+            coordinatesEnd
+        };
+
+        try {
+            const response = await service.post("/rutas", nuevaRuta);
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+            navigate("/error500");
+        }
+    };
+
+    return <div></div>;
 }
 
 export default FormCrearRuta;
